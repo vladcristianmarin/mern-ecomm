3a0d851e968ba44a1ad1c4d7c9090957184f516b
vladcristianmarin <vladmarin34@gmail.com>
2022-03-03 17:49:49 +0200

    production-ready

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,13 @@ app.get('/api/config/paypal', (_req, res) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+	app.get('*', (_req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	);
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
